42b33acda4282ac265bc094698bb16a2e84869c8
Paul Chen <lancevdance@gmail.com>
2025-11-19 10:59:59 +0800

    Add project files.

---
+++ PrintDmpStack/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Runtime.InteropServices;
+using System.Text.Json;
+using PrintDmpStack;
+
+if (args.Length < 3)
+{
+    if (!File.Exists("params.txt") || File.ReadAllLines("params.txt") is not { Length: >= 3 } lines)
+    {
+        Console.Error.WriteLine("""
+            Need at least 3 positional args, or use a params.txt file to specify them in separated lines (cuz the launchProfile.json is buggy):
+
+                0. The path to the .dmp file
+
+                1. Image paths:
+                    Semicolon-separated list of folders to the possible modules in the crashed application.
+                    This arg is directly passed to dbgeng.
+
+                2. Symbol paths:
+                    Semicolon-separated list of folders to the possible symbols (.pdb files) for the crashed application.
+                    This arg is directly passed to dbgeng.
+
+                3. Output format:
+                    0 - compacted json, that's default for automation
+                    1 - pretty-printed json
+                    2 - human readable
+            """);
+
+        return -2;
+    }
+    else
+    {
+        args = lines;
+    }
+}
+
+try
+{
+    using var da = DumpAnalyzer.Create(dumpFile: args[0], imagePaths: args[1], symbolPaths: args[2]);
+    using var stdOut = Console.OpenStandardOutput();
+
+    var stack = da.GetExceptionStackTrace().ToArray();
+
+    if (args.Length < 4 || args[3] == "0")
+    {
+        JsonSerializer.Serialize(stdOut, stack, DumpStackJsonSerializationContext.Default.DumpStackFrameArray);
+    }
+    else if (args[3] == "1")
+    {
+#pragma warning disable CA1869 // Cache and reuse 'JsonSerializerOptions' instances
+        var options = new JsonSerializerOptions
+        {
+            TypeInfoResolver = DumpStackJsonSerializationContext.Default,
+            WriteIndented = true,
+        };
+#pragma warning restore CA1869 // Cache and reuse 'JsonSerializerOptions' instances
+
+        JsonSerializer.Serialize(stdOut, stack, options);
+    }
+    else if (args[3] == "2")
+    {
+        foreach (var frame in stack)
+        {
+            Console.WriteLine(frame.ToHumanReadable());
+        }
+    }
+
+    return 0;
+}
+catch (Exception e)
+{
+    Console.Error.Write(e);
+
+    return e is COMException com ? com.ErrorCode : -4;
+}
